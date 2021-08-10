5e405b923466c0fd57b87f8c37cc2a96664605ef
Kevin Andrew Jett <kjett@bbn.com>
2021-08-10 15:48:42 -0400

    Update run scripts with correct data location and Python version
    Pointing to Prasanna's checked-out copy of "sotto-voce-corpus".  This repository
    seems to have been removed from Github, so we'll want to hang on to this copy.
    
    Switched to use version of Python in the path.  That way, you can activate a
    virtual environment that has everything set up prior to running and use its
    Python.

--- senone_classifier/run_eval.sh
+++ senone_classifier/run_eval.sh
@@ -1,25 +1,22 @@
 #!/bin/bash
 
 model_path='exp/temp_2X200/final.pth.tar'
-eval_scp_file_name='train_full_nodup_cv10/feats.scp' 
-eval_scp_path='/nfs/mercury-13/u123/dbagchi/ASR_utt'
-post_fname='posterior_hub5_2X200'
+eval_scp_file_name='train_full_nodup_cv10/feats.scp'
+eval_label_scp_file_name='train_full_nodup_cv10/phone.ctm2.scp' 
+eval_scp_path='/nfs/raid83/u13/caml/users/pmuthuku_ad/CSL_exps/exps/senone_classifier/sotto-voce-corpus'
 
 input_dim=13
 output_dim=9096
 fc_nodes=200
 hidden_layers=2
-#input_dim=440
-#output_dim=2
-#fc_nodes=1024
-#fc_layers=1
 
 
 
-/nfs/mercury-13/u123/dbagchi/anaconda3/envs/pytorch_gpu_deblin/bin/python eval.py --model_path $model_path \
+python eval.py \
+ --model_path $model_path \
  --eval_scp_file_name $eval_scp_file_name \
  --eval_scp_path $eval_scp_path \
- --post_fname $post_fname \
+ --eval_label_scp_file_name $eval_label_scp_file_name \
  --input_dim $input_dim \
  --output_dim $output_dim \
  --fc_nodes $fc_nodes \
